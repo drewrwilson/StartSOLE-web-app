b6052074d4fa3e21bc27d555541a3c172279026f
justin <jsduffy@gmail.com>
2018-06-15 16:32:30 -0400

    handlebars helpers, other stuff, hooking things to view

--- server.js
+++ server.js
@@ -25,26 +25,26 @@ const password = process.argv[3]; //2nd parameter after node test-parse.js USERN
 var sessionToken = null;
 Parse.User.enableUnsafeCurrentUser();
 Parse.User.logIn(username, password)
-  .done((user)=>{
+    .done((user)=>{
     Parse.Cloud.run("platform.set", {
-        app	: "web",
-        build: "2.0",
-        info: "development for now"
-    }).then(data=>{
-      console.log('Logged in!');
-      console.log('\n---\n');
-  		sessionToken = Parse.User.current().getSessionToken();
-  		console.log('current user token: ', sessionToken);
-  		console.log('\n---\n');
-      console.log('Check out StartSOLE2 locally at:\n');
-      console.log('http://localhost:'+port+'?'+sessionToken.replace(':', '='));
-  		console.log('\n---\n');
-    })
+    app	: "web",
+    build: "2.0",
+    info: "development for now"
+}).then(data=>{
+    console.log('Logged in!');
+console.log('\n---\n');
+sessionToken = Parse.User.current().getSessionToken();
+console.log('current user token: ', sessionToken);
+console.log('\n---\n');
+console.log('Check out StartSOLE2 locally at:\n');
+console.log('http://localhost:'+port+'?'+sessionToken.replace(':', '='));
+console.log('\n---\n');
+})
 
-  })
-  .catch((err)=>{
+})
+.catch((err)=>{
     console.log('error logging in', err);
-  })
+})
 
 hbs.registerHelper('ifEquals',
     function(a, b, opts) {
@@ -56,14 +56,41 @@ hbs.registerHelper('ifEquals',
     }
 );
 
+hbs.registerHelper('select', function(selected, options) {
+    return options.fn(this).replace(
+        new RegExp(' value=\"' + selected + '\"'),
+        '$& selected="selected"');
+});
+
+hbs.registerHelper('check', function(checked, options) {
+
+    // var mapObj = {"material.poster_paper":"dog",dog:"goat",goat:"cat"};
+    //
+    // var re = new RegExp(Object.keys(mapObj).join("|"),"gi");
+    // str = str.replace(re, function(matched){
+    //     return mapObj[matched];
+    // });
+    //
+    //
+    //
+    // console.log("CHECKED");
+    // console.log(checked);
+    // for(var i=0; i<checked.length; i++){
+    //     return options.fn(this).replace(
+    //         new RegExp(' id=\"' + checked[i] + '\"'),
+    //         '$& checked');
+    // }
+
+});
+
 // set the view engine
 app.set('view engine', 'hbs');
 
 // configure the view engine
 app.engine('hbs', hbs.express4({
-  defaultLayout: __dirname + '/views/layouts/default.hbs',
-  partialsDir: __dirname + '/views/partials',
-  layoutsDir: __dirname + '/views/layouts'
+    defaultLayout: __dirname + '/views/layouts/default.hbs',
+    partialsDir: __dirname + '/views/partials',
+    layoutsDir: __dirname + '/views/layouts'
 }));
 
 // configure app to use bodyParser()
@@ -83,256 +110,248 @@ var router = express.Router();              // get an instance of the express Ro
 //       redirect to a intro screen. Or after completing your 5 SOLE, give some
 //       nice encouraging message, etc etc
 router.route('/')
-  .get((req, res) => {
+    .get((req, res) => {
 
     var homeData = {soles: [],questions:[]};
 
-    Controllers.Question.getAll("sessionToken")
-      .then((questions)=>{
-        console.log('questions', questions);
-        console.log('got all questions:');
-        console.log(questions);
-        console.log('---');
-        homeData.questions.mine = questions.questions;
+Controllers.Question.getAll("sessionToken")
+    .then((questions)=>{
+    console.log('questions', questions);
+console.log('got all questions:');
+console.log(questions);
+console.log('---');
+homeData.questions.mine = questions.questions;
 
-        Controllers.Question.getFavorites(sessionToken.r)
-          .then((favoriteQuestions)=>{
-            console.log('got fav questions:');
-            console.log(favoriteQuestions);
-            console.log('---');
-            homeData.questions.favorites = favoriteQuestions;
+Controllers.Question.getFavorites(sessionToken.r)
+    .then((favoriteQuestions)=>{
+    console.log('got fav questions:');
+console.log(favoriteQuestions);
+console.log('---');
+homeData.questions.favorites = favoriteQuestions;
 
-            res.render('home', homeData); //display view with question data
-          })
-          .catch((err)=>{
-            console.log('error getting fav questions!', err);
-          })
-      })
-      .catch((err)=>{
-        console.log('error getting questions!', err);
-      });
+res.render('home', homeData); //display view with question data
+})
+.catch((err)=>{
+    console.log('error getting fav questions!', err);
+})
+})
+.catch((err)=>{
+    console.log('error getting questions!', err);
+});
 
-  });
+});
 
 
 // static route for History of SOLE
 router.route('/history')
-  .get(function(req, res) {
-    res.render('history');
-  });
+    .get(function(req, res) {
+        res.render('history');
+    });
 
 // static route for History of SOLE
 router.route('/how')
-  .get(function(req, res) {
-    res.render('how-to-sole');
-  });
+    .get(function(req, res) {
+        res.render('how-to-sole');
+    });
 
 // static route for History of SOLE
 router.route('/terms-of-use')
-  .get(function(req, res) {
-    res.render('terms-of-use');
-  });
+    .get(function(req, res) {
+        res.render('terms-of-use');
+    });
 
 // static route for History of SOLE
 router.route('/privacy')
-  .get(function(req, res) {
-    res.render('privacy');
-  });
+    .get(function(req, res) {
+        res.render('privacy');
+    });
 
 // routes for resources
 router.route('/resources')
-  .get(function(req, res) {
-    var resources = Controllers.Resource.getAll();
-    res.render('resources', {resources: resources});
-  });
+    .get(function(req, res) {
+        var resources = Controllers.Resource.getAll();
+        res.render('resources', {resources: resources});
+    });
 
 // static route for community map
 router.route('/map')
-  .get(function(req, res) {
-    res.render('map');
-  });
+    .get(function(req, res) {
+        res.render('map');
+    });
 
 // routes for profile
 // ----------------------------------------------------
 router.route('/profile')
 
-  // profile view
-  .get((req, res) => {
+// profile view
+    .get((req, res) => {
     Controllers.User.getProfileData(sessionToken)
-      .then((profileData) => {
-        res.render('profile', profileData);
-      });
+    .then((profileData) => {
+    res.render('profile', profileData);
+});
 
-  });
+});
 
 // routes for user registration
 // ----------------------------------------------------
 router.route('/register')
 
-  // profile view
-  .get(function(req, res) {
-    res.render('register');
-  });
-
-  // routes for user registration
-  // ----------------------------------------------------
-  router.route('/login')
-
-    // profile view
+// profile view
     .get(function(req, res) {
-      res.render('login');
+        res.render('register');
+    });
+
+// routes for user registration
+// ----------------------------------------------------
+router.route('/login')
+
+// profile view
+    .get(function(req, res) {
+        res.render('login');
     });
 
 // routes for soles
 // ----------------------------------------------------
 router.route('/soles')
 
-  // get all the soles (accessed at GET http://localhost:8080/api/soles)
-  .get(function(req, res) {
-    Controllers.Sole.getAll(sessionToken)
-      .then(soles=>{
-        console.log('All soles', JSON.stringify(soles));
-        res.render('soles', soles);
-      })
-      .catch(err=>{
-        console.log('oops! error!', err);
-      })
-
-  });
-
-  // on routes that end in /soles/:sole_id
-  // ----------------------------------------------------
-  router.route('/soles/:id')
-      // get the sole with that id (accessed at GET http://localhost:8080/api/soles/:sole_id)
-      .get(function(req, res) {
-        Controllers.Sole.getByID(req.params.id, sessionToken)
-          .then((singleSole) => {
-            //in case the id of the sole is invalid
-            console.log(JSON.stringify(singleSole.sole));
-            res.render('soles-single', singleSole);
-          })
-          .catch((err)=>{console.log('error!', err);})
-      });
-  // on routes that end in /soles/:sole_id
-  // ----------------------------------------------------
-  router.route('/soles/:id/edit')
-      // get the sole with that id (accessed at GET http://localhost:8080/api/soles/:sole_id)
-      .get(function(req, res) {
-        Controllers.Sole.getByID(req.params.id, sessionToken).then((singleSole) => {
-          res.render('soles-add', singleSole);
-        });
-      });
-
-  // on routes that end in /soles/add/
-  // ----------------------------------------------------
-  router.route('/sole-create')
-      // view for adding a new sole
-      .get(function(req, res) {
-        res.render('soles-add');
-      });
-
-  //this route is just for testing:
-  router.route('/test-sole-create')
-      // view for adding a new sole
-      .get(function(req, res) {
-        var exampleSole = {
-          state: "planned",//always planned
-          subject: "top.lifeskills",
-          grade: "edu.12",
-          tag: "World History",
-          question: "Example question text",
-          planned_date: "20180428182142",
-          planned_duration: 60.0,
-          num_groups: 5,
-          plan_state: 6,
-          target_observations: [
-            "session.observation.collaborating",
-            "session.observation.technology",
-            "session.observation.respectful"
-          ],
-          materials: [
-            "material.poster_paper",
-            "material.physical",
-            "material.other"
-          ],
-          num_students: 20,
-          num_devices: 5,
-          group_organization: true,
-          group_sharing: false,
-          "self-assessment": true,
-          content_objective: "objective.content.deepen",
-          use_app: true,//collected from the form
-          facilitation : {
-            timer: {
-              question: {
-                "duration":120
-              },
-              investigate: {
-                "duration":2280
-              },
-              review: {
-                "duration":120
-              },
-              close: {
-                "duration":600
-              }
-            }
-          }
-        }
-        Controllers.Sole.add(exampleSole).then(soleID=>{
-          console.log(soleID);
-        })
-      });
-
-  // on routes that end in /questions
-  // ----------------------------------------------------
-  router.route('/questions')
-    // get all the soles (accessed at GET http://localhost:8080/questions)
+// get all the soles (accessed at GET http://localhost:8080/api/soles)
     .get(function(req, res) {
+        Controllers.Sole.getAll(sessionToken)
+            .then(soles=>{
+            console.log('All soles', JSON.stringify(soles));
+        res.render('soles', soles);
+    })
+        .catch(err=>{
+            console.log('oops! error!', err);
+    })
 
-      if (req.query.q) {
-        Controllers.Question.findByText(req.query.q, sessionToken).then((foundQuestions) => {
-          console.log(JSON.stringify(foundQuestions));
-          res.render('questions', foundQuestions);
-        });
-      } else if (req.query.tags) {
-        Controllers.Question.findByTags(req.query.tags, sessionToken).then((foundQuestions) => {
-          //todo probably need to do some processing on tags to convert it from a string to an array of tags
-          console.log(JSON.stringify(foundQuestions));
-          res.render('questions', foundQuestions);
-        });
-      } else {
-      Controllers.Question.getAll(sessionToken).then((allQuestions)=>{
-        res.render('questions', allQuestions);
-      });
-    }
     });
 
-    //add a question
-    router.route('/questions/add')
-      .get(function(req, res) {
-        res.render('questions-add');
-      });
-    // on routes that end in /questions/:id
-    // ----------------------------------------------------
-    router.route('/questions/:id')
-      // get the question data with a given id
-      .get(function(req, res) {
-        Controllers.Question.getByID(req.params.id).then((questionData) => {
-          console.log(JSON.stringify(questionData));
-          res.render('questions-single', questionData);
-        });
-      });
+// on routes that end in /soles/:sole_id
+// ----------------------------------------------------
+router.route('/soles/:id')
+// get the sole with that id (accessed at GET http://localhost:8080/api/soles/:sole_id)
+    .get(function(req, res) {
+        Controllers.Sole.getByID(req.params.id, sessionToken)
+            .then((singleSole) => {
+            //in case the id of the sole is invalid
+            console.log(JSON.stringify(singleSole.sole));
+        res.render('soles-single', singleSole);
+    })
+        .catch((err)=>{console.log('error!', err);})
+    });
+// on routes that end in /soles/:sole_id
+// ----------------------------------------------------
+router.route('/soles/:id/edit')
+// get the sole with that id (accessed at GET http://localhost:8080/api/soles/:sole_id)
+    .get(function(req, res) {
+        Controllers.Sole.getByID(req.params.id, sessionToken).then((singleSole) => {
+            console.log("single sole!!!");
+            console.log(singleSole);
+            res.render('soles-add', singleSole);
+    });
+    });
 
-    router.route('/questions/:id/favorite')
-      // favorite a question with a given id
-      .get(function(req, res) {
-        Controllers.Question.favorite(req.params.id).then((questionData) => {
-          console.log(JSON.stringify(questionData));
-          // res.render('questions-single', questionData);
-          res.redirect('/questions/'+req.params.id);
+// on routes that end in /soles/add/
+// ----------------------------------------------------
+router.route('/sole-create')
+// view for adding a new sole
+    .get(function(req, res) {
+        res.render('soles-add');
+    });
+
+//this route is just for testing:
+router.route('/test-sole-create')
+// view for adding a new sole
+    .get(function(req, res) {
+        var exampleSole = {
+            state: "planned",//always planned REMEMBER TO SET THIS
+            plan_state: 6, //REMEMBER TO SET THIS
+            subject: "top.english",
+            grade: "edu.12",
+            class_label: "World History", //could be empty
+            question: "What if there were no rules or laws in the local community?",
+            question_id: "EFePpFvBuo",
+            planned_date: "Jun 18, 2018",
+            planned_time: "09:59 AM",
+            planned_duration: 100,
+            num_groups: 5,
+            target_observations: [
+                "session.observation.collaborating",
+                "session.observation.technology",
+                "session.observation.respectful"
+            ],
+            materials: [
+                "material.poster_paper",
+                "material.physical",
+                "material.other"
+            ],
+            num_students: 20,
+            num_devices: 5,
+            group_organization: true,
+            group_sharing: false,
+            self_assessment: true,
+            content_objective: "objective.content.deepen",
+            use_app: true,//collected from the form
+            time_question : 300,
+            time_investigate : 4350,
+            time_review : 750,
+            time_close : 600,
+        };
+        Controllers.Sole.add(exampleSole).then(soleID=>{
+            console.log(soleID);
+    })
+    });
+
+// on routes that end in /questions
+// ----------------------------------------------------
+router.route('/questions')
+// get all the soles (accessed at GET http://localhost:8080/questions)
+    .get(function(req, res) {
+
+        if (req.query.q) {
+            Controllers.Question.findByText(req.query.q, sessionToken).then((foundQuestions) => {
+                console.log(JSON.stringify(foundQuestions));
+            res.render('questions', foundQuestions);
         });
-      });
+        } else if (req.query.tags) {
+            Controllers.Question.findByTags(req.query.tags, sessionToken).then((foundQuestions) => {
+                //todo probably need to do some processing on tags to convert it from a string to an array of tags
+                console.log(JSON.stringify(foundQuestions));
+            res.render('questions', foundQuestions);
+        });
+        } else {
+            Controllers.Question.getAll(sessionToken).then((allQuestions)=>{
+                res.render('questions', allQuestions);
+        });
+        }
+    });
+
+//add a question
+router.route('/questions/add')
+    .get(function(req, res) {
+        res.render('questions-add');
+    });
+// on routes that end in /questions/:id
+// ----------------------------------------------------
+router.route('/questions/:id')
+// get the question data with a given id
+    .get(function(req, res) {
+        Controllers.Question.getByID(req.params.id).then((questionData) => {
+            console.log(JSON.stringify(questionData));
+        res.render('questions-single', questionData);
+    });
+    });
+
+router.route('/questions/:id/favorite')
+// favorite a question with a given id
+    .get(function(req, res) {
+        Controllers.Question.favorite(req.params.id).then((questionData) => {
+            console.log(JSON.stringify(questionData));
+        // res.render('questions-single', questionData);
+        res.redirect('/questions/'+req.params.id);
+    });
+    });
 
 // REGISTER OUR ROUTES -------------------------------
 app.use('/', router);

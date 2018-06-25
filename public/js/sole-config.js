88c14da2abd36d7d9b9ef739c5539fac86c6c1dc
Thanks No Thanks <drew.r.wilson@gmail.com>
2018-06-25 12:02:29 -0400

    staging server data

--- public/js/sole-config.js
+++ public/js/sole-config.js
@@ -1,1 +1,5 @@
-var soleConfig = {"serverUrl":"http://localhost:1339/soleapp","appId":"Hcwnq8U7xN4Z2bXcSBdvv4bjfRNKCpPSahXgeq9xRp"}
+// local
+// var soleConfig = {"serverUrl":"http://localhost:1339/soleapp","appId":"Hcwnq8U7xN4Z2bXcSBdvv4bjfRNKCpPSahXgeq9xRp"}
+
+// staging
+var soleConfig = {"serverUrl":"https://api.staging.startsole.net/sole/","appId":"Hcwnq8U7xN4Z2bXcSBdvv4bjfRNKCpPSahXgeq9xRp"}
--- sole-config.js
+++ sole-config.js
@@ -1,5 +1,5 @@
-var soleConfig = module.exports = {};
+// local
+// var soleConfig = {"serverUrl":"http://localhost:1339/soleapp","appId":"Hcwnq8U7xN4Z2bXcSBdvv4bjfRNKCpPSahXgeq9xRp"}
 
-// soleConfig.serverUrl = "http://localhost:1339/soleapp";
-soleConfig.serverUrl = "https://api.staging.startsole.net/sole/";
-soleConfig.appId = "Hcwnq8U7xN4Z2bXcSBdvv4bjfRNKCpPSahXgeq9xRp";
+// staging
+var soleConfig = {"serverUrl":"https://api.staging.startsole.net/sole/","appId":"Hcwnq8U7xN4Z2bXcSBdvv4bjfRNKCpPSahXgeq9xRp"}

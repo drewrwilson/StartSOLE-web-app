a439d2de3ceb5e39e280861394a5f890a87e8eb1
Thanks No Thanks <drew.r.wilson@gmail.com>
2018-06-25 17:16:16 -0400

    WIP question filtering. not done yet, just committing because a piece of it is currently working

--- public/js/questions.js
+++ public/js/questions.js
@@ -18,6 +18,8 @@ Parse.serverURL = soleConfig.serverUrl;
 var sesh         = $('#sesh').val(), //get the sesh token from the DOM
     sessionToken = 'r:' + sesh;      //convert sesh token to full sessionToken string
 
+var standardPickerIndex = 0;
+
 function getGrades(subject){
   return Parse.Cloud.run('webapp.getGrades', {
     subject: subject || false
@@ -30,6 +32,12 @@ function getStandards(rdn, grade){
   	grade: grade || false
   });
 }
+
+function makeStandardPickerHTML (short, rdn) {
+  var html = '<div class="input-field col s12 m6 new-standard-picker"><label for="refine-0" class="active">Domain</label><select id="refine-0" class="browser-default" ><option label="short 1" value="1">short1</option><option label="short 2" value="2">short2</option><option label="short 3" value="3">short3</option><option label="short 4" value="4">short4</option></div>';
+  return html;
+}
+
 
 //when the user changes the subject, get the corresponding grades
 $('#subject').change(function (){
@@ -51,20 +59,62 @@ $('#subject').change(function (){
   })
 })
 
-//when the user changes the grade, get the corresponding standards
 $('#grade').change(function (){
-  var grade = $(this).val(),
+  var grade = $('#grade').val(),
       rdn = $('#subject').val();
-      console.log('grade', grade);
-      console.log('rdn', rdn);
+
   getStandards(rdn, grade).then(standards=>{
     console.log(standards);
+
+    var source   = $("#standard-picker-template").html(),
+        template = Handlebars.compile(source),
+        html     = '';
+      console.log({standards: standards});
+    html = template({standards: standards});
+    $('#standard-picker').append(html)
+
   })
 })
 
-//when the user changes the subject, get the corresponding grades
-// TODO:
+//when the user changes the grade, get the corresponding standards
+function itChanged (element){
+  console.log('IT CHANGED');
+  var grade = $('#grade').val(),
+      rdn = element.val();
 
+  getStandards(rdn, grade).then(standards=>{
+    console.log(standards);
+
+    if (standards.length > 0) {
+      var source   = $("#standard-picker-template").html(),
+          template = Handlebars.compile(source),
+          html     = '';
+        console.log({standards: standards});
+      html = template({standards: standards});
+      $('#standard-picker').append(html)
+    }
+
+
+
+  })
+}
+
+//when the user changes the standard, get the corresponding standards, infinite
+// TODO:
+// $('.new-standard-picker').change(function (){
+//   var rdn   = $(this).val(),
+//       grade = $('#grade').val();
+//
+//   grade = 'edu.6';
+//   rdn = 'asn.s114340d'; //hardcoded for testing Geometry
+//
+//   console.log('rdn', rdn);
+//   getStandards(rdn, grade).then(standards=>{
+//     console.log(standards);
+//     $('#standard-picker').append(makeStandardPickerHTML())
+//   })
+//
+// })
 
 //whenever any standard picker select changes, get the questions that are tagged with the corresponding standards
 $('.standard-picker').change(function (){
@@ -76,7 +126,7 @@ $('.standard-picker').change(function (){
 
   Parse.Cloud.run('webapp.findQuestionByTags', {
     tags: standards,
-    sessionToken: sessionToken 
+    sessionToken: sessionToken
   }).then(response=>{
     //Ok, now that we have the questions with a given tag, let's add them to the DOM
 

c6d73ba2fa107ddf9172e62e3a846cfb5a6becf6
Thanks No Thanks <drew.r.wilson@gmail.com>
2018-06-28 09:03:44 -0400

    WIP registration

---
+++ public/js/register.js
@@ -0,0 +1,29 @@
+$( "#initial-registration-form").submit(function (event) {
+
+  event.preventDefault();
+  var first_name  = $('#first_name').val(),
+      last_name   = $('#last_name').val(),
+      email       = $('#email').val(),
+      password    = $('#password').val();
+
+  Parse.Cloud.run("user.add", {
+    name: first_name + " " + last_name,
+    email: email,
+    pw: password
+  }).then(data=>{
+    console.log('data', data);
+    // login with the new user and get a session token
+    // redirect to complete-profile with session token
+    return Parse.User.logIn(email, password)
+      .done(function (user){
+        console.log('login success!');
+        var sessionToken = user.attributes.sessionToken;
+        var sesh = user.attributes.sessionToken.slice(2);
+        $('#sesh').val(sesh);
+        $('#firstname').val(first_name);
+        $('#lastname').val(last_name);
+        $( "#final-registration-form" ).submit();
+      })
+  });
+
+});

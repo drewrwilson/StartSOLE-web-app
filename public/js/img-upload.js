f57af33d242332c3897067c71dbdd06a2c390e3b
Thanks No Thanks <drew.r.wilson@gmail.com>
2018-06-29 14:59:42 -0400

    add dropzone

--- public/js/img-upload.js
+++ public/js/img-upload.js
@@ -7,18 +7,15 @@ Parse.serverURL = soleConfig.serverUrl;
 //   uploadImage();
 // })
 
-// uploads images in materials file input
-function uploadImages () {
+// uploads an image to parse file input
+function uploadImage(file) {
   console.log('uploadImages() ran');
   var name = "photo.jpg";
   var sessionToken = 'r:'+$("#sesh");
   var soleID = 'MgNHVDLpJ3';//hardcoded now, update with soleID
 
-  var fileUploadControl = $("#photoUpload")[0];
-  for (var i = 0; i < fileUploadControl.files.length; i++) {
-
-    var file = fileUploadControl.files[i]
-    name = fileUploadControl.files[i].name;
+    name =file.name;
+    console.log('name: ' +name);
     console.log('file found. name:' + name);
     var parseFile = new Parse.File(name, file);
 
@@ -41,15 +38,22 @@ function uploadImages () {
       // The file either could not be read, or could not be saved to Parse.
       console.log('error uploading image', error);
     });
-  }
+
 }
 
-
-//
-// function sendImgToParse (id, image, sessionToken) {
-//   return Parse.Cloud.run('webapp.saveImage', {
-//     id: id,
-//     imageFile: image,
-//     sessionToken: sessionToken
-//   })
-// }
+Dropzone.options.myAwesomeDropzone = {
+  url: "/api/upload-sole-image",
+  // paramName: "image", // The name that will be used to transfer the file
+  maxFilesize: 10, // MB
+  parallelUploads: 10,
+  maxFiles: 10,
+  autoProcessQueue: false,
+  addRemoveLinks: false,
+  // hiddenInputContainer: '#dropzone-'+$scope.suffix,
+  dictDefaultMessage: 'Click or drop files here to upload.  No more than 10 photos please!',
+  acceptedFiles: 'image/*',
+  accept: function(file, done) {
+    uploadImage(file);
+    console.log(file);
+  }
+};

6b756e13e67e9623fbc1886f4feef6faaa24785e
Thanks No Thanks <drew.r.wilson@gmail.com>
2018-06-29 15:10:56 -0400

    get sole id

--- public/js/img-upload.js
+++ public/js/img-upload.js
@@ -2,54 +2,45 @@
 Parse.initialize(soleConfig.appId);
 Parse.serverURL = soleConfig.serverUrl;
 
-//trigger image upload on change
-// $('#photoUpload').change(function(e) {
-//   uploadImage();
-// })
 
 // uploads an image to parse file input
 function uploadImage(file) {
-  console.log('uploadImages() ran');
-  var name = "photo.jpg";
-  var sessionToken = 'r:'+$("#sesh");
-  var soleID = 'MgNHVDLpJ3';//hardcoded now, update with soleID
 
-    name =file.name;
-    console.log('name: ' +name);
-    console.log('file found. name:' + name);
-    var parseFile = new Parse.File(name, file);
+  var name         = file.name,
+      sessionToken = 'r:'+$("#sesh"),
+      soleID       = $('#soleID').val();
 
-    parseFile.save().then(function(file) {
-      //maybe disable submit button until finished uploading
-      console.log('saved file, now uploading to parse');
+  var parseFile = new Parse.File(name, file);
 
-      //send image to parse server
-      Parse.Cloud.run('webapp.saveImage', {
-        id: soleID,
-        imageFile: file,
-        sessionToken: sessionToken
-      }).then(response=>{
-        console.log('image uploaded', response);
-        //maybe reenable submit button now since it's finished uploading
-      }).catch(error=>{
-        console.log('oops error calling cloud code! error: ', error);
-      })
-    }, function(error) {
-      // The file either could not be read, or could not be saved to Parse.
-      console.log('error uploading image', error);
-    });
+  parseFile.save().then(function(file) {
+    //maybe disable submit button until finished uploading
+    console.log('saved file, now uploading to parse');
+
+    //send image to parse server
+    Parse.Cloud.run('webapp.saveImage', {
+      id: soleID,
+      imageFile: file,
+      sessionToken: sessionToken
+    }).then(response=>{
+      console.log('image uploaded', response);
+      //maybe reenable submit button now since it's finished uploading
+    }).catch(error=>{
+      console.log('oops error calling cloud code! error: ', error);
+    })
+  }, function(error) {
+    // The file either could not be read, or could not be saved to Parse.
+    console.log('error uploading image', error);
+  });
 
 }
 
 Dropzone.options.myAwesomeDropzone = {
-  url: "/api/upload-sole-image",
-  // paramName: "image", // The name that will be used to transfer the file
+  url: "/api/upload-sole-image",//this isn't used
   maxFilesize: 10, // MB
   parallelUploads: 10,
   maxFiles: 10,
   autoProcessQueue: false,
   addRemoveLinks: false,
-  // hiddenInputContainer: '#dropzone-'+$scope.suffix,
   dictDefaultMessage: 'Click or drop files here to upload.  No more than 10 photos please!',
   acceptedFiles: 'image/*',
   accept: function(file, done) {

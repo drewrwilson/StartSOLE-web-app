2ec8c48c4fa8df0526224a50d1b712859c6aa210
Thanks No Thanks <drew.r.wilson@gmail.com>
2018-07-06 13:04:33 -0400

    oops accidentally committed. reverting

--- public/js/sole-config.js
+++ public/js/sole-config.js
@@ -1,5 +1,5 @@
 // local
-var soleConfig = {"serverUrl":"http://localhost:1339/soleapp","appId":"Hcwnq8U7xN4Z2bXcSBdvv4bjfRNKCpPSahXgeq9xRp"}
+// var soleConfig = {"serverUrl":"http://localhost:1339/soleapp","appId":"Hcwnq8U7xN4Z2bXcSBdvv4bjfRNKCpPSahXgeq9xRp"}
 
 // staging
-// var soleConfig = {"serverUrl":"https://api.staging.startsole.net/sole/","appId":"Hcwnq8U7xN4Z2bXcSBdvv4bjfRNKCpPSahXgeq9xRp"}
+var soleConfig = {"serverUrl":"https://api.staging.startsole.net/sole/","appId":"Hcwnq8U7xN4Z2bXcSBdvv4bjfRNKCpPSahXgeq9xRp"}

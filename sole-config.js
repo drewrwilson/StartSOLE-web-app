a60250fa503644678535a3928eeeed0067203c43
Thanks No Thanks <drew.r.wilson@gmail.com>
2018-07-06 13:37:30 -0400

    adding production to sole-config.js

--- sole-config.js
+++ sole-config.js
@@ -1,9 +1,13 @@
 var soleConfig = module.exports = {};
 
 //for local
-// soleConfig.serverUrl = "http://localhost:1339/soleapp";
+soleConfig.serverUrl = "http://localhost:1339/soleapp";
 
 // uncomment for staging
-soleConfig.serverUrl = "https://api.staging.startsole.net/sole/";
+// soleConfig.serverUrl = "https://api.staging.startsole.net/sole/";
+
+// uncomment for production
+// soleConfig.serverUrl = "https://api.startsole.net/sole/";
+
 
 soleConfig.appId = "Hcwnq8U7xN4Z2bXcSBdvv4bjfRNKCpPSahXgeq9xRp";

40e0fe223e638c440f1052eba509a19b03cec81b
Thanks No Thanks <drew.r.wilson@gmail.com>
2018-07-10 13:15:37 -0400

    put a ring on it

--- sole-config.js
+++ sole-config.js
@@ -1,19 +1,19 @@
 var soleConfig = module.exports = {};
 
-var client = process.env.CLIENT || 'regular';        // can be "hrsa" or nothing
+var ring = process.env.RING || 'regular';        // can be "hrsa" or nothing
 var environment = process.env.ENVIRONMENT || 'local';
 
 soleConfig.logo = 'logo.png'; //default
 soleConfig.tagline = 'Let learning happen'; //default
-soleConfig.client = 'startsole'; //default
+soleConfig.ring = 'startsole'; //default
 soleConfig.serverUrl = 'http://localhost:1339/soleapp'; //defaultly set to local
 soleConfig.appId = 'Hcwnq8U7xN4Z2bXcSBdvv4bjfRNKCpPSahXgeq9xRp'; //same for all environments
 soleConfig.facebookAppID = '283486318802722' //default for local testing
 
-//change some values if the client is HRSA
-if (client.toLowerCase() == 'hrsa') {
+//change some values if the ring is HRSA
+if (ring.toLowerCase() == 'hrsa') {
   soleConfig.logo = 'logo-hrsa.png';
-  soleConfig.client = 'hrsa';
+  soleConfig.ring = 'hrsa';
   // soleConfig.facebookAppID = ''; //
 }
 
@@ -23,5 +23,5 @@ if (environment.toLowerCase() == 'production') {
   soleConfig.facebookAppID = '495194740686129'; //production facebook app ID
 } else if (environment.toLowerCase() == 'staging') {
   soleConfig.serverUrl = 'https://api.staging.startsole.net/sole/';
-  // soleConfig.facebookAppID = ''; //
+  // soleConfig.facebookAppID = ''; // same as local testing
 }

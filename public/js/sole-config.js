8fcebefa7654d6d39a4d01d7d732d311588f406f
Thanks No Thanks <drew.r.wilson@gmail.com>
2018-07-10 15:08:46 -0400

    remove sole-config from frontend because now its a handlebars template

--- public/js/sole-config.js
+++
@@ -1,9 +0,0 @@
-// local
-var soleConfig = {"serverUrl":"http://localhost:1339/soleapp","appId":"Hcwnq8U7xN4Z2bXcSBdvv4bjfRNKCpPSahXgeq9xRp"}
-
-// console.log('in sole-config.js on frontend. I have this variable. config.logo: ', '{{config.logo}}');
-// staging
-// var soleConfig = {"serverUrl":"https://api.staging.startsole.net/sole/","appId":"Hcwnq8U7xN4Z2bXcSBdvv4bjfRNKCpPSahXgeq9xRp"}
-
-// staging
-// var soleConfig = {"serverUrl":"https://api.startsole.net/sole/","appId":"Hcwnq8U7xN4Z2bXcSBdvv4bjfRNKCpPSahXgeq9xRp"}

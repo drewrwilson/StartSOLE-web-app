55d5612ce8f6f071c2558f84b56b4d0758c8e75f
justin <jsduffy@gmail.com>
2018-07-19 11:24:10 -0400

    testing SOLE limit

--- controllers/sole-controller.js
+++ controllers/sole-controller.js
@@ -19,7 +19,7 @@ Sole.getByID = function (id, sessionToken) {
 Sole.getAll = function (sessionToken) {
   return Parse.Cloud.run('webapp.getAllMySoles', {
     offset: 0,
-    limit: 100,
+    limit: 5,
 		sessionToken: sessionToken
 	})
 }

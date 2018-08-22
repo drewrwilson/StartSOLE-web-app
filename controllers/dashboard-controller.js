b39ce716c38a439144596cf3dae520be43966cae
justin <jsduffy@gmail.com>
2018-08-22 14:55:07 -0400

    added 2017 data for comparison purposes on dashboard.  Shouldn't do more here until we check with Frans re: Statistics.js

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -8,7 +8,18 @@ Parse.serverURL = soleConfig.serverUrl;
 
 //returns data to build a simple line graph of users by month
 Dashboard.getUsersByMonth = function (sessionToken) {
+  var result = {};
   return Parse.Cloud.run('webapp.getDashboardData', {
+    year: 2018,
     sessionToken: sessionToken
-  })
+  }).then(arr18=>{
+      result["data18"] = arr18;
+      return Parse.Cloud.run('webapp.getDashboardData', {
+        year: 2017,
+        sessionToken: sessionToken
+      })
+  }).then(arr17=>{
+    result["data17"] = arr17;
+    return result;
+  });
 }

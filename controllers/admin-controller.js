4f8b8ce525510d32dd46b12836beeb54807d4ec9
Drew <drew.r.wilson@gmail.com>
2019-01-17 14:27:41 -0500

    Make stubs in anticipation of question approval

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -10,7 +10,7 @@ class Admin {
     //gets data for any SOLE sessions that haven't been approved or rejected yet
     static getPendingSoles (sessionToken) {
         return Parse.Cloud.run('webapp.getUnapprovedSoles', {
-            limit: 999w,
+            limit: 999,
             sessionToken: sessionToken
         }).then(solesJson => {
             return Promise.resolve(solesJson);
@@ -42,50 +42,50 @@ class Admin {
             sessionToken: sessionToken
         });
     };
-
-    //get any questions that haven't been approved yet
-    //stub Todo: make this
-    static getPendingQuestions (sessionToken) {
-        return Parse.Cloud.run('webapp.getUnapprovedSoles', {
-            limit: 999w,
-            sessionToken: sessionToken
-        }).then(solesJson => {
-            return Promise.resolve(solesJson);
-        });
-    }
-    /*
-    approve a given question
-    param:
-        questionId - string id of question
-        comment - string comment from approver (to be used in an email to the submitter)
-        tags - array of string tags
-        sessionToken - string sessionToken
-     */
-    //stub Todo: make this
-    static approveQuestion (questionId, comment, sessionToken) {
-
-        // return Parse.Cloud.run('webapp.approveQuestion', {
-        //     id: soleId,
-        //     comment: comment,
-        //     sessionToken: sessionToken
-        // });
-    };
-
-    /*
-    reject a given question
-    param:
-        questionId - string id of question
-        comment - string comment from rejector (to be used in an email to the submitter)
-        sessionToken - string sessionToken
-     */
-    //stub Todo: make this
-    static rejectQuestion (questionId, comment, sessionToken) {
-        // return Parse.Cloud.run('webapp.rejectQuestion', {
-        //     id: questionId,
-        //     comment: comment,
-        //     sessionToken: sessionToken
-        // });
-    };
+    //
+    // //get any questions that haven't been approved yet
+    // //stub Todo: make this
+    // static getPendingQuestions (sessionToken) {
+    //     // return Parse.Cloud.run('webapp.getUnapprovedQuestions', {
+    //     //     limit: 999,
+    //     //     sessionToken: sessionToken
+    //     // }).then(solesJson => {
+    //     //     return Promise.resolve(solesJson);
+    //     // });
+    // }
+    // /*
+    // approve a given question
+    // param:
+    //     questionId - string id of question
+    //     comment - string comment from approver (to be used in an email to the submitter)
+    //     tags - array of string tags
+    //     sessionToken - string sessionToken
+    //  */
+    // //stub Todo: make this
+    // static approveQuestion (questionId, comment, sessionToken) {
+    //
+    //     // return Parse.Cloud.run('webapp.approveQuestion', {
+    //     //     id: soleId,
+    //     //     comment: comment,
+    //     //     sessionToken: sessionToken
+    //     // });
+    // };
+    //
+    // /*
+    // reject a given question
+    // param:
+    //     questionId - string id of question
+    //     comment - string comment from rejector (to be used in an email to the submitter)
+    //     sessionToken - string sessionToken
+    //  */
+    // //stub Todo: make this
+    // static rejectQuestion (questionId, comment, sessionToken) {
+    //     // return Parse.Cloud.run('webapp.rejectQuestion', {
+    //     //     id: questionId,
+    //     //     comment: comment,
+    //     //     sessionToken: sessionToken
+    //     // });
+    // };
 
 }
 

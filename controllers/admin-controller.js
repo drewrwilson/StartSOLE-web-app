50bf9f16423392be5d2533b0ced926cdfb03146f
Drew <drew.r.wilson@gmail.com>
2019-01-17 14:14:03 -0500

    Make stubs for question approval

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -10,24 +10,13 @@ class Admin {
     //gets data for any SOLE sessions that haven't been approved or rejected yet
     static getPendingSoles (sessionToken) {
         return Parse.Cloud.run('webapp.getUnapprovedSoles', {
-            limit: 999,
+            limit: 999w,
             sessionToken: sessionToken
         }).then(solesJson => {
             return Promise.resolve(solesJson);
         });
     }
 
-
-    // static getPendingSole (soleId, sessionToken) {
-    //     return Parse.Cloud.run('webapp.getSoleByIdApproveReject', {
-    //         id: soleId,
-    //         sessionToken: sessionToken
-    //     }).then(soleJson => {
-    //         return Parse.Promise.as(soleJson);
-    //     });
-    // };
-
-    //approve a sole and records comments, sends email async
     /*
     param:
         soleId - string
@@ -52,6 +41,50 @@ class Admin {
             comment: comment,
             sessionToken: sessionToken
         });
+    };
+
+    //get any questions that haven't been approved yet
+    //stub Todo: make this
+    static getPendingQuestions (sessionToken) {
+        return Parse.Cloud.run('webapp.getUnapprovedSoles', {
+            limit: 999w,
+            sessionToken: sessionToken
+        }).then(solesJson => {
+            return Promise.resolve(solesJson);
+        });
+    }
+    /*
+    approve a given question
+    param:
+        questionId - string id of question
+        comment - string comment from approver (to be used in an email to the submitter)
+        tags - array of string tags
+        sessionToken - string sessionToken
+     */
+    //stub Todo: make this
+    static approveQuestion (questionId, comment, sessionToken) {
+
+        // return Parse.Cloud.run('webapp.approveQuestion', {
+        //     id: soleId,
+        //     comment: comment,
+        //     sessionToken: sessionToken
+        // });
+    };
+
+    /*
+    reject a given question
+    param:
+        questionId - string id of question
+        comment - string comment from rejector (to be used in an email to the submitter)
+        sessionToken - string sessionToken
+     */
+    //stub Todo: make this
+    static rejectQuestion (questionId, comment, sessionToken) {
+        // return Parse.Cloud.run('webapp.rejectQuestion', {
+        //     id: questionId,
+        //     comment: comment,
+        //     sessionToken: sessionToken
+        // });
     };
 
 }
